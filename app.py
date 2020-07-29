1f20ee6ad5e9fbbac847c99d6e9315d9dd8decda
IYERADavid <iyeradavid@gmail.com>
2020-07-29 18:39:53 +0200

    final commit

--- app.py
+++ app.py
@@ -229,5 +229,10 @@ def home_page():
         return redirect(url_for('login'))
     return render_template('home.html')
 
+@app.route('/home',methods=['GET','POST'])
+def andrews_page():
+    if request.method == 'GET':
+        return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(debug=True)    
